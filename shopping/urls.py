c5e9d2bd85d3474273b3f7456c5c04de413cf907
NickLont <nlontorfos@hotmail.com>
2016-05-25 16:48:22 +0300

    GETs to POSTs ready

--- shopping/urls.py
+++ shopping/urls.py
@@ -5,9 +5,8 @@ from django.contrib.auth.decorators import login_required
 
 app_name = 'shopping'
 urlpatterns = [
-    url(r'^add/(?P<isbn>[0-9]+)$', login_required(views.add), name='shopping-cart-add'),
-    url(r'^remove/(?P<isbn>[0-9]+)$', login_required(views.remove), name='shopping-cart-remove'),
+    url(r'^add/$', login_required(views.add), name='shopping-cart-add'),
+    url(r'^remove/$', login_required(views.remove), name='shopping-cart-remove'),
     url(r'^show/$', login_required(views.show), name='shopping-cart-show'),
-    # url(r'^checkout/$', views.checkout, name='shopping-cart-checkout'),
 ]	
 
